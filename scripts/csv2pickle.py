8a1f0cd1b10a4d10e4f71d459520301b0c0a5a61
tturowski <twturowski@gmail.com>
2021-01-27 13:51:41 +0000

    csv2pickle added

--- scripts/csv2pickle.py
+++ scripts/csv2pickle.py
@@ -9,10 +9,14 @@ parser = argparse.ArgumentParser(usage=usage, formatter_class=argparse.ArgumentD
 files = parser.add_argument_group('Options for input files')
 files.add_argument("-f", dest="csv_file", help="SAM file",
                  metavar="FILE", default=None)
+files.add_argument("--gzip", dest="compression", help="gzip compression",action="store_true")
 args = parser.parse_args()
 
 path = os.getcwd()+"/"
 filename=args.csv_file
 
 df = pd.read_csv(path+filename, sep=",",index_col=0,header=0)
-df.to_pickle(path+filename.replace(".csv",".pickle"))
+if args.compression == True:
+    df.to_pickle(path + filename.replace(".csv", ".pcl.gz"),compression='gzip')
+else:
+    df.to_pickle(path+filename.replace(".csv",".pcl"))
