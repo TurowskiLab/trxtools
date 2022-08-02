ad7d11f691c912e410a69303070397a490f6b544
Tomasz W. Turowski <twturowski@gmail.com>
2022-08-02 14:57:49 +0200

    SAM2profiles working for genome, both strands.

--- TTools/SAM2profiles.py
+++ TTools/SAM2profiles.py
@@ -71,6 +71,7 @@ def countRead(i=tuple()):
     match = groupCIGAR(cigar_string)
     # stripping substitutions
     strip_shift = tostripCIGARfive(match)
+    if not strip_shift: strip_shift = 0
     match = stripSubstitutions(match)
 
     # outputs
@@ -138,7 +139,7 @@ def countDeletion(i=tuple(), expand=0):
 
 ### main functions ###
 def transcript2profile(l=[], length=int()):
-    '''Takes list of tuples position,CIGARstring) and generates profile'''
+    '''Takes list of tuples position,CIGARstring) and generates profile. Works with entire reads, for both strands.'''
     list_hits = [countRead(i) for i in l]  # list of lists
     hits = [item for sublist in list_hits for item in sublist]  # transforms to flat list
     # returning the profile
@@ -147,7 +148,7 @@ def transcript2profile(l=[], length=int()):
 
 
 def transcript2profileDeletions(l=[], expand=0, length=int()):
-    '''Takes list of tuples position,CIGARstring) and generates profile'''
+    '''Takes list of tuples position,CIGARstring) and generates profile. Not tested for MINUS strand.'''
     list_hits = [countDeletion(i, expand=expand) for i in l]  # list of lists
     hits = [item for sublist in list_hits for item in sublist]  # transforms to flat list
     # returning the profile
@@ -155,7 +156,10 @@ def transcript2profileDeletions(l=[], expand=0, length=int()):
     return profile.reindex(pd.RangeIndex(length + 1)).fillna(0)  # fills spaces with 0 counts
 
 
+
 def reads2profile(name=str(), dirPath=str(), df_details=pd.DataFrame()):
+    '''Takes list of aligned reads and transform to profile for each transctipt. Tested only for PLUS strand.
+    '''
     cols = ['score', 'name', 'position', 'CIGAR', 'sequence', 'details']
     df_input = pd.read_csv(dirPath + "/" + name + ".tab", sep="\t", names=cols)
 
@@ -178,6 +182,7 @@ def reads2profile(name=str(), dirPath=str(), df_details=pd.DataFrame()):
 
 
 def reads2profileDeletions(name=str(), dirPath=str(), df_details=pd.DataFrame(), expand=5):
+    '''Tested only for PLUS strand.'''
     cols = ['score', 'name', 'position', 'CIGAR', 'sequence', 'details']
     df_input = pd.read_csv(dirPath + "/" + name + ".tab", sep="\t", names=cols)
 
@@ -213,6 +218,7 @@ def reads2profileDeletions(name=str(), dirPath=str(), df_details=pd.DataFrame(),
 
 def sam2profiles(filename="", path='', geneList=[], toClear='', df_details=pd.DataFrame(),
                  deletions=False, expand=5, pickle=False,chunks=0):
+    '''Tested only for PLUS strand.'''
     # making working directory
     name = filename.replace(".sam", "")
     if toClear:
@@ -238,6 +244,7 @@ def sam2profiles(filename="", path='', geneList=[], toClear='', df_details=pd.Da
     os.chdir(dirPath)
     for i in chunkList:
         geneListFileName = "geneList_" + str(i) + ".tab"
+        #FLAG 0 and 256 for single end reads, aligned, forward match
         command = "grep -v ^@ ../" + filename + " | grep -f "+geneListFileName+\
                   " | awk -F'\t' 'BEGIN{OFS = FS} $2==0||$2==256{print $2,$3, $4, $6, $10, $12}' > " + name+"_" + str(i) + ".tab"
         tt.methods.bashCommand(command)
@@ -278,5 +285,124 @@ def sam2profiles(filename="", path='', geneList=[], toClear='', df_details=pd.Da
     # clean
     os.chdir(path)
     shutil.rmtree(name + timestamp)
+
+    print("Done.")
+
+def reads2genome(name=str(), dirPath=str(), df_details=pd.DataFrame()):
+    '''Works for both strands, extensive testing needed.'''
+
+    cols = ['score', 'name', 'position', 'CIGAR', 'sequence', 'details']
+    df_input_fwd = pd.read_csv(dirPath + "/" + name + "_fwd.tab", sep="\t", names=cols)
+    df_input_rev = pd.read_csv(dirPath + "/" + name + "_rev.tab", sep="\t", names=cols)
+
+    output_df_fwd = pd.DataFrame()
+    output_df_rev = pd.DataFrame()
+    log = []
+
+    for n, df in df_input_fwd.groupby('name'):
+        try:
+            length = df_details.loc[n]['length']
+            l = list(zip(df['position'].astype(int), df['CIGAR']))
+            s1 = transcript2profile(l, length=length)
+            output_df_fwd = output_df_fwd.append(s1.rename(n))
+
+            log.append(n + " - FWD profile generated successfully")
+        except:
+            log.append(n + " - FWD profile FAILED")
+            
+    for n, df in df_input_rev.groupby('name'):
+        try:
+            length = df_details.loc[n]['length']
+            l = list(zip(df['position'].astype(int), df['CIGAR']))
+            s1 = transcript2profile(l, length=length)
+            output_df_rev = output_df_rev.append(s1.rename(n))
+
+            log.append(n + " - REV profile generated successfully")
+        except:
+            log.append(n + " - REV profile FAILED")
+
+    return output_df_fwd, output_df_rev, log
+
+
+def sam2genome(filename="", path='', geneList=[], toClear='', df_details=pd.DataFrame(),
+                pickle=False,chunks=0):
+    '''Works for both strands, extensive testing needed.'''
+    # making working directory
+    name = filename.replace(".sam", "")
+    if toClear:
+        name = name.replace(toClear, "")
+    timestamp = tt.methods.timestampRandomInt()
+    dirPath = path + name + timestamp
+    os.makedirs(dirPath)
+
+    #geneList = chromosome list from the @SQ header of SAM file'
+    command = "grep @SQ " + filename + " | cut -f2 | sed 's/SN\://' > " + dirPath + "/" + name + "_chr.list"
+    tt.methods.bashCommand(command)
+    geneList = tt.methods.read_list(dirPath + "/" + name + "_chr.list")
+    #chromosome lengths from SAM header
+    command = "grep @SQ " + filename + " | cut -f4 | sed 's/LN\://' > " + dirPath + "/" + name + "_chr.len"
+    tt.methods.bashCommand(command)
+    geneLen = tt.methods.read_list(dirPath + "/" + name + "_chr.len")
+    
+    df_details = pd.DataFrame(data={"chrName" : geneList, "length" : geneLen}).set_index('chrName')
+    df_details['length'] = df_details['length'].astype(int)
+    
+    # tempfiles
+    ##list of files if red as chunks
+    if chunks > 0:
+        chunkList = [i for i in range(0,len(geneList)+1,chunks)]
+    else:
+        chunkList = [0]
+        chunks = len(geneList)+1
+
+    ##genes means chromosomes for this function
+    genes = pd.DataFrame(pd.Series(geneList))
+    for i in chunkList:
+        geneListFileName = "/geneList_" + str(i) + ".tab"
+        genes[i:i+chunks].to_csv(dirPath + geneListFileName, sep='\t', header=False,index=False)
+        
+    ##Select FWD and REV reads from SAM file
+    os.chdir(dirPath)
+    
+    for i in chunkList:
+        geneListFileName = "geneList_" + str(i) + ".tab"
+        
+        print
+        #FLAG 0 and 256 for single end reads, aligned, forward match
+        command = "grep -v ^@ ../" + filename + " | grep -f " + geneListFileName +\
+                  " | awk -F'\t' 'BEGIN{OFS = FS} $2==0||$2==256{print $2,$3, $4, $6, $10, $12}' > "+\
+                  name + "_" + str(i) + "_fwd.tab"
+        tt.methods.bashCommand(command)
+        
+        #FLAG 16 and 272 for single end reads, aligned, forward match
+        command = "grep -v ^@ ../" + filename + " | grep -f " + geneListFileName +\
+                  " | awk -F'\t' 'BEGIN{OFS = FS} $2==16||$2==272{print $2,$3, $4, $6, $10, $12}' > "+\
+                  name + "_" + str(i) + "_rev.tab"
+        tt.methods.bashCommand(command)
+    
+    tt.methods.bashCommand("cat "+name+"*fwd.tab > " + name +"_fwd.tab")
+    tt.methods.bashCommand("cat "+name+"*rev.tab > " + name +"_rev.tab")
+
+    print("Reads selected.")
+
+    #Reads to profiles
+    df_profiles_fwd, df_profiles_rev, log = reads2genome(name=name, dirPath=dirPath,df_details=df_details)
+    
+    
+    #save output
+    if pickle==True:
+        df_profiles_fwd.to_pickle(path + name + "_PROFILES_reads_fwd.pcl")
+        df_profiles_rev.to_pickle(path + name + "_PROFILES_reads_rev.pcl")
+    elif pickle==False:
+        df_profiles_fwd.to_csv(path + name + "_PROFILES_reads_fwd.csv")
+        df_profiles_rev.to_csv(path + name + "_PROFILES_reads_rev.csv")
+    #save log
+    with open(path + name + "_PROFILES_reads.log", "w") as log_file:
+        for row in log:
+            log_file.write(str(row) + '\n')
+
+    # clean
+    os.chdir(path)
+#     shutil.rmtree(name + timestamp)
 
     print("Done.")
