2773d45c6da44f8316ceafda4b0feef8c06d0554
Tomasz Turowski <twturowski@gmail.com>
2022-10-18 10:42:12 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import setuptools
 
 setuptools.setup(
     name='TTools',
-    version='0.1.0',
+    version='0.2.0',
     author="Tomasz W. Turowski, Jan Mikołajczyk",
     author_email="twturowski@gmail.com",
     description='Python tools facilitating bioinformatic analysis of nascent transcripts and transcriptomic data.',
