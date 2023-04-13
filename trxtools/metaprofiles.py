6f6ab3effa350fe87ce08b49e5b05adf786571cf
j-miko <jm@MOTHER-BASE>
2023-04-13 16:54:23 +0200

    Added option to align matrices to 3'end of features

--- trxtools/metaprofiles.py
+++ trxtools/metaprofiles.py
@@ -11,12 +11,12 @@ def read_bed(bed_path):
     return bed_df
 
 ### level -2
-def get_bw_data(bed_row, bw, flank_5=0, flank_3=0):
+def get_bw_data(bed_row, bw, flank_5=0, flank_3=0, align_3end=False):
     '''
     Retrieve BigWig scores for positions in a given region, optionally including flanks of given length.
     '''
     if bed_row[5] == '+':
-        return pd.Series(bw.values(bed_row[0], bed_row[1]-flank_5, bed_row[2]+flank_3))
+        outseries = pd.Series(bw.values(bed_row[0], bed_row[1]-flank_5, bed_row[2]+flank_3))
     if bed_row[5] == '-':
         # flip orientation on minus strand
         # keep the index so pandas doesn't flip it again
@@ -24,7 +24,11 @@ def get_bw_data(bed_row, bw, flank_5=0, flank_3=0):
         rev_index = outseries.index
         outseries = outseries.iloc[::-1]
         outseries.index = rev_index
-        return outseries
+    if align_3end:
+        outseries.index = outseries.index - flank_5 - (bed_row[2] - bed_row[1])
+    else:
+        outseries.index = outseries.index - flank_5
+    return outseries
 
 ### level -1    
 def bed_split_strands(bed_df):
@@ -38,16 +42,16 @@ def bed_split_strands(bed_df):
     bed_minus = bed_df[bed_df[5] == "-"]
     return bed_plus, bed_minus
 
-def matrix_from_bigwig(bw_path, bed_df, flank_5=0, flank_3=0, fill_na=True, pseudocounts=None, normalize_libsize=True):
+def matrix_from_bigwig(bw_path, bed_df, flank_5=0, flank_3=0, fill_na=True, pseudocounts=None, normalize_libsize=True, align_3end=False):
     '''
     Get matrix with bigwig scores for all regions in a bed df.
     Matrix rows correspond to regions in BED.
     Columns correpond to nucleotide positions in regions + flanks.
     '''
     bw = pyBigWig.open(bw_path)
-    out_df = bed_df.apply(get_bw_data, bw=bw, flank_5=flank_5, flank_3=flank_3, axis=1)
-    if out_df.columns[0] != 0:
-        out_df.columns = out_df.columns[::-1]
+    out_df = bed_df.apply(get_bw_data, bw=bw, flank_5=flank_5, flank_3=flank_3, align_3end=align_3end, axis=1)
+    # if out_df.columns[0] != 0:
+    #     out_df.columns = out_df.columns[::-1]
     out_df['region'] = bed_df[3]
     out_df = out_df.set_index('region')
     if fill_na:
@@ -77,7 +81,7 @@ def join_strand_matrices(plus_dict, minus_dict):
     return out_dict
 
 ### level 0
-def get_multiple_matrices(bw_paths_plus, bw_paths_minus, bed_df, flank_5=0, flank_3=0, fill_na=True, pseudocounts=None, normalize_libsize=True):
+def get_multiple_matrices(bw_paths_plus, bw_paths_minus, bed_df, flank_5=0, flank_3=0, fill_na=True, pseudocounts=None, normalize_libsize=True, align_3end=False):
     """Get score matrices for positions in given regions (with optional flanks) from multiple BigWig files.
     Matrix rows correspond to regions in BED.
     Columns correpond to nucleotide positions in regions + flanks.
@@ -98,16 +102,18 @@ def get_multiple_matrices(bw_paths_plus, bw_paths_minus, bed_df, flank_5=0, flan
     :type pseudocounts: float, optional
     :param normalize_libsize: normalization to library size (sum of scores in a bigwig file), defaults to True
     :type normalize_libsize: bool, optional
+    :param align_3end: if True, align profiles at the 3'end of features. Otherwise align at 5'end, defaults to False
+    :type align_3end: bool, optional
     :return:  A dictionary containing score matrices for individual BigWig files. Dictionary keys are BigWig file names.
     :rtype: dict
     """    
     bed_plus, bed_minus = bed_split_strands(bed_df)
     plus_dict = {}
     for bw in bw_paths_plus:
-        plus_dict[bw] = matrix_from_bigwig(bw_path=bw, bed_df=bed_plus, flank_5=flank_5, flank_3=flank_3, fill_na=fill_na, pseudocounts=pseudocounts, normalize_libsize=normalize_libsize)
+        plus_dict[bw] = matrix_from_bigwig(bw_path=bw, bed_df=bed_plus, flank_5=flank_5, flank_3=flank_3, fill_na=fill_na, pseudocounts=pseudocounts, normalize_libsize=normalize_libsize, align_3end=align_3end)
     minus_dict = {}
     for bw in bw_paths_minus:
-        minus_dict[bw] = matrix_from_bigwig(bw_path=bw, bed_df=bed_minus, flank_5=flank_5, flank_3=flank_3, fill_na=fill_na, pseudocounts=pseudocounts, normalize_libsize=normalize_libsize)
+        minus_dict[bw] = matrix_from_bigwig(bw_path=bw, bed_df=bed_minus, flank_5=flank_5, flank_3=flank_3, fill_na=fill_na, pseudocounts=pseudocounts, normalize_libsize=normalize_libsize, align_3end=align_3end)
     return join_strand_matrices(plus_dict, minus_dict)
 
 def metaprofile(matrix_dict, agg_type='mean', normalize_internal=False):
